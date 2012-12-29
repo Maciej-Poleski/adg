e2059c3e504055e823beda3d911ca87575a35b16
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2012-12-29 16:42:42 +0100

    Client to Slave stage 2/4

--- src/ConnCS/ClientToSlaveRequest.cxx
+++ src/ConnCS/ClientToSlaveRequest.cxx
@@ -5,6 +5,8 @@
 #include "ClientToSlaveRequest.hxx"
 
 #include <algorithm>
+#include <boost/asio/write.hpp>
+#include <boost/asio/read.hpp>
 
 #include "../shared/Request.hxx"
 
@@ -66,6 +68,11 @@ ClientToSlaveRequest::discussionsToCommit() const
 void ClientToSlaveRequest::sendTo(boost::asio::ip::tcp::socket& socket) const
 {
     using namespace detail;
+    {
+        byte v[1]= {0};
+        boost::asio::write(socket,boost::asio::buffer(v),
+                           boost::asio::transfer_exactly(1));
+    }
     writeToSocket(static_cast<std::uint32_t>(_discussionsToUpdate.size()),
                   socket);
     for(const auto o : _discussionsToUpdate)
@@ -84,4 +91,57 @@ void ClientToSlaveRequest::sendTo(boost::asio::ip::tcp::socket& socket) const
             writeToSocket(oo,socket);
         }
     }
+}
+
+ClientToSlaveRequest ClientToSlaveRequest::receiveFrom(
+    boost::asio::ip::tcp::socket& socket)
+{
+    using namespace detail;
+    {
+        byte v[1];
+        boost::asio::read(socket,boost::asio::buffer(v),
+                          boost::asio::transfer_exactly(1));
+        if(*v!=0)
+            throw std::runtime_error("Client requested unsupported protocol");
+    }
+    ClientToSlaveRequest result;
+    try
+    {
+        {
+            auto count=readFromSocket< std::uint32_t >(socket);
+            for(std::size_t i=0; i<count; ++i)
+            {
+                auto id=readFromSocket< std::uint32_t >(socket);
+                auto version=readFromSocket< std::uint32_t >(socket);
+                result.addDiscussionToUpdate(id,version);
+            }
+        }
+        {
+            auto count=readFromSocket< std::uint32_t >(socket);
+            for(std::size_t i=0; i<count; ++i)
+            {
+                auto id=readFromSocket< std::uint32_t >(socket);
+                checkDiscussionID(id);
+                auto c=readFromSocket< std::uint32_t >(socket);
+                if(c==0)
+                {
+                    throw std::runtime_error("Client commited 0 new posts."
+                                             " This is forbidden."
+                                            );
+                }
+                for(std::size_t i=0; i<c; ++i)
+                {
+                    auto post=readFromSocket<Post>(socket);
+                    post.check();
+                    result.addPostToCommit(id,post);
+                }
+            }
+        }
+    }
+    catch(std::logic_error e)
+    {
+        throw std::runtime_error(std::string("Received malformed object: ")+
+                                 e.what());
+    }
+    return result;
 }
